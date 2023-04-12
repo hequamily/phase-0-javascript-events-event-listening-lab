428585543861607c82e83343c989267fae817572
Harrison Quamily <quamilyh@gmail.com>
2023-04-12 11:03:29 -0400

    newevents

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function addingEventListener() {
-    const input = document.getElementById('button');
-input.addEventListener('click', function() {
-  alert('I was clicked!');
-});
+// function addingEventListener() {
+
+//   const input = document.getElementById('button');
+//   input.addEventListener('click', function() {
+//     alert('I was clicked!');
+//     // console.log(`hi`)
+//   });
+// }
+
+const input = document.getElementById('button');
+
+function clickAlert() {
+  console.log('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+//     const input = document.getElementById('button');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
